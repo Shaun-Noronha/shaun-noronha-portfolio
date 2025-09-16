c1106f33f9c7e28602c05434967958a3b8f4dff5
Shaun Noronha <shaun2001xx@gmail.com>
2025-09-16 11:03:46 -0400

    Porfolio Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "popins-black": "popins-black",
+        "popins-bold": "popins-bold",
+        "popins-extrabold": "popins-extrabold",
+        "popins-extralight": "popins-extralight",
+        "popins-medium": "popins-medium",
+        "popins-semibold": "popins-semibold",
+        "popins-thin": "popins-thin",
+        "popins-regular": "popins-regular",
+      },
+    },
+  },
+  plugins: [],
+};
